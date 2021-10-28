0b4d760806ad6dd6f2bdcbdab20f0cccd7bcfca8
ang-jason8 <angjason896+github@gmail.com>
2021-10-28 12:28:18 +0800

    my-changes

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const server = require('./server');
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
   // eslint-disable-next-line no-console
-  
+  console.log('j650n', port);
   console.log('UP AND RUNNING @', port);
 });
